7f509c2757e37349a8845efe56eded974f559b08
Filip Sjölander <filip.sjolander@gmail.com>
2024-03-06 17:00:28 +0100

    settings and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:timeedit/firebase_options.dart';
 import 'package:timeedit/screens/home.dart';
 import 'package:timeedit/screens/booking.dart';
 import 'package:timeedit/screens/maps.dart';
 import 'package:timeedit/screens/settings.dart';
 import 'package:timeedit/widgets/navbar.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key ?? const Key(''));
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   int currentPageIndex = 0;
+  ThemeMode _themeMode = ThemeMode.system; // Initialize with system theme
 
-  final List<Widget> screens = [
-    HomeScreen(),
-    BookingScreen(qrCode: "test"),
-    MapsScreen(),
-    SettingsScreen(),
-    // Add more screens here
-  ];
+  late List<Widget> screens;
 
   void onNavBarPageSelected(int index) {
     setState(() {
@@ -32,13 +35,40 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void _handleThemeChanged(ThemeMode mode) {
+    setState(() {
+      _themeMode = mode; // Update theme mode
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    screens = [
+      HomeScreen(),
+      BookingScreen(qrCode: "test"),
+      MapsScreen(),
+      SettingsScreen(
+          onThemeChanged:
+              _handleThemeChanged), // Initialize with settings screen
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.white,
+        brightness: Brightness.light,
+      )),
+      darkTheme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+        seedColor: Color.fromARGB(255, 0, 0, 1),
+        brightness: Brightness.dark,
+      )),
+      themeMode: _themeMode, // Use the updated theme mode
       home: Scaffold(
         body: screens[currentPageIndex],
         bottomNavigationBar: NavBar(
--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,17 +1,66 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  final Function(ThemeMode) onThemeChanged;
+
+  SettingsScreen({Key? key, required this.onThemeChanged}) : super(key: key);
+
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  ThemeMode _themeMode = ThemeMode.system;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Center(
-        child: Text(
-          'Settings Screen',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 20),
+        children: <Widget>[
+          ListTile(
+            title: Text(
+              'Theme',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+          ),
+          RadioListTile(
+            title: Text('Follow System'),
+            value: ThemeMode.system,
+            groupValue: _themeMode,
+            onChanged: (value) {
+              setState(() {
+                _themeMode = value as ThemeMode;
+              });
+              widget.onThemeChanged(_themeMode); // Notify parent widget
+            },
+          ),
+          RadioListTile(
+            title: Text('Dark Mode'),
+            value: ThemeMode.dark,
+            groupValue: _themeMode,
+            onChanged: (value) {
+              setState(() {
+                _themeMode = value as ThemeMode;
+              });
+              widget.onThemeChanged(_themeMode); // Notify parent widget
+            },
+          ),
+          RadioListTile(
+            title: Text('Light Mode'),
+            value: ThemeMode.light,
+            groupValue: _themeMode,
+            onChanged: (value) {
+              setState(() {
+                _themeMode = value as ThemeMode;
+              });
+              widget.onThemeChanged(_themeMode); // Notify parent widget
+            },
+          ),
+        ],
       ),
     );
   }
