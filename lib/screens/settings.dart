3327df6b07252781d6b9350db451f26829fdd0df
Phozel <adamwi@student.chalmers.se>
2024-03-19 11:37:07 +0100

    Did some practice in settings settings screen
    didn't remove anything, just commented the already existing code and replaced it with my new practice

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -8,28 +8,43 @@ import 'package:provider/provider.dart'; // new
 import '../app_state.dart'; // new
 import 'package:go_router/go_router.dart';
 
+
 class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Settings'),
-        ),
-        body: Center(
-            child: Consumer<ApplicationState>(
-          builder: (context, appState, _) => appState.loggedIn
-              ? ElevatedButton(
-                  onPressed: () async {
-                    await FirebaseAuth.instance.signOut();
-                  },
-                  child: Text('Sign out'),
-                )
-              : ElevatedButton(
-                  onPressed: () {
-                    context.push('/sign-in');
-                  },
-                  child: Text('Sign in'),
-                ),
-        )));
+      body: Container(
+        alignment: Alignment.center,
+        child: const Text('Settings Screen'),
+      ),
+    );
   }
+
+  
 }
+
+  //Det gamla Filip hade i settings
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //       appBar: AppBar(
+  //         title: Text('Settings')),
+  //       body: Center(
+  //           child: Consumer<ApplicationState>(
+  //         builder: (context, appState, _) => appState.loggedIn
+  //             ? ElevatedButton(
+  //                 onPressed: () async {
+  //                   await FirebaseAuth.instance.signOut();
+  //                 },
+  //                 child: Text('Sign out'),
+  //               )
+  //             : ElevatedButton(
+  //                 onPressed: () {
+  //                   context.push('/sign-in');
+  //                 },
+  //                 child: Text('Sign in'),
+  //               ),
+  //       )));
+  // }
