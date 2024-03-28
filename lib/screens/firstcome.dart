65f158b8ae3162b6afa0d25af543ed38de7d0419
qorux <91456201+qorux@users.noreply.github.com>
2024-03-28 09:29:35 +0100

    first come first serve

--- lib/screens/firstcome.dart
+++ lib/screens/firstcome.dart
@@ -19,26 +19,26 @@ class FirstCome extends StatelessWidget {
             SizedBox(height: 20), // Add spacing between the title and accordions
             AccordionWidget(
               title: 'Maskin',
-              content: 'Content for Accordion 1',
-              color: Colors.green, // Set background color to green
+              content: ['Room 1', 'Room 2', 'Room 3'], // Example content list
+              backgroundColor: Color(0xFFBFD5BC), // Set custom color
             ),
             SizedBox(height: 16), // Add spacing between accordion items
             AccordionWidget(
               title: 'Kemi',
-              content: 'Content for Accordion 2',
-              color: Colors.green, // Set background color to green
+              content: ['Room 4', 'Room 5', 'Room 6'], // Example content list
+              backgroundColor: Color(0xFFBFD5BC), // Set custom color
             ),
             SizedBox(height: 16), // Add spacing between accordion items
             AccordionWidget(
               title: 'Fysik',
-              content: 'Content for Accordion 3',
-              color: Colors.green, // Set background color to green
+              content: ['Room 7', 'Room 8', 'Room 9'], // Example content list
+              backgroundColor: Color(0xFFBFD5BC), // Set custom color
             ),
             SizedBox(height: 16), // Add spacing between accordion items
             AccordionWidget(
               title: 'SB',
-              content: 'Content for Accordion 4',
-              color: Colors.green, // Set background color to green
+              content: ['Room 10', 'Room 11', 'Room 12'], // Example content list
+              backgroundColor: Color(0xFFBFD5BC), // Set custom color
             ),
           ],
         ),
@@ -49,10 +49,10 @@ class FirstCome extends StatelessWidget {
 
 class AccordionWidget extends StatefulWidget {
   final String title;
-  final String content;
-  final Color color; // Background color
+  final List<String> content;
+  final Color backgroundColor; // Background color
 
-  AccordionWidget({required this.title, required this.content, required this.color});
+  AccordionWidget({required this.title, required this.content, required this.backgroundColor});
 
   @override
   _AccordionWidgetState createState() => _AccordionWidgetState();
@@ -64,14 +64,14 @@ class _AccordionWidgetState extends State<AccordionWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: widget.color, // Set the background color
+      color: widget.backgroundColor, // Set the background color
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ListTile(
             title: Text(
               widget.title,
-              style: TextStyle(color: Colors.white), // Set text color to white for contrast
+              style: TextStyle(color: Colors.black), // Set text color to black
             ),
             onTap: () {
               setState(() {
@@ -80,20 +80,139 @@ class _AccordionWidgetState extends State<AccordionWidget> {
             },
             trailing: Icon(
               _expanded ? Icons.keyboard_arrow_up : Icons.keyboard_arrow_down,
-              color: Colors.white, // Set icon color to white for contrast
+              color: Colors.black, // Set icon color to black
             ),
           ),
           if (_expanded)
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                widget.content,
-                style: TextStyle(color: Colors.white), // Set text color to white for contrast
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: widget.content
+                  .asMap()
+                  .entries
+                  .map(
+                    (entry) => GestureDetector(
+                      onTap: () {
+                        _showRoomInfoDialog(context, entry.value);
+                      },
+                      child: Container(
+                        color: entry.key.isOdd ? Color(0xFFD9D9D9) : Color(0xFFEFECE7),
+                        child: ListTile(
+                          title: Text(
+                            entry.value,
+                            style: TextStyle(color: Colors.black), // Set text color to black
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
             ),
-          Divider(height: 0, color: Colors.white), // Set divider color to white for contrast
+          Divider(height: 0, color: Colors.black), // Set divider color to black
+        ],
+      ),
+    );
+  }
+
+  void _showRoomInfoDialog(BuildContext context, String roomName) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CustomDialog(
+          roomName: roomName,
+          onClose: () {
+            Navigator.of(context).pop();
+          },
+        );
+      },
+    );
+  }
+}
+
+class CustomDialog extends StatelessWidget {
+  final String roomName;
+  final VoidCallback onClose;
+
+  const CustomDialog({Key? key, required this.roomName, required this.onClose}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+      backgroundColor: Color(0xFFF0F0F0), // Set background color to F0F0F0
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  roomName,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+                ),
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: onClose,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildInfoRow('House:', 'Sample House'),
+                _buildInfoRow('Size:', 'Sample Size'),
+                _buildInfoRow('Amenities:', 'Sample Amenities'),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add action for Get Directions button here
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Color(0xFFBFD5BC)), // Set background color
+                      ),
+                      child: Text('Get Directions?'),
+                    ),
+                    OutlinedButton(
+                      onPressed: onClose,
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(color: Color(0xFFEFECEC)), // Set border color
+                      ),
+                      child: Text('Close'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          Text(
+            label,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(width: 8),
+          Text(value),
         ],
       ),
     );
   }
 }
+
+
+
