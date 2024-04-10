bc2448b9301d36aa206645f51d9d5dcc5a85332c
qorux <91456201+qorux@users.noreply.github.com>
2024-04-10 13:58:01 +0200

    layouten ser rätt bra ut nu

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -79,102 +79,115 @@ class TitleSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Color(0xFFBFD5BC), // Background color for the titles
-              borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)), // Rounded top corners
+      child: Container( // Wrap Column with Container
+        height: 400, // Set a finite height for the container
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Looking for a free room right now?',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
             ),
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text(
-                      'Room',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.black), // Text color
+            SizedBox(height: 8), // Add spacing between text and green box
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xFFBFD5BC), // Background color for the titles
+                borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)), // Rounded top corners
+              ),
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        'Room',
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.black), // Text color
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 46.0),
-                    child: Text(
-                      'Building',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.black), // Text color
+                  Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 46.0),
+                      child: Text(
+                        'Building',
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.black), // Text color
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 4,
-                  child: Padding(
-                    padding: EdgeInsets.only(right: 16.0),
-                    child: Text(
-                      'Available until',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.black), // Text color
-                      textAlign: TextAlign.right,
-                      overflow: TextOverflow.ellipsis,
+                  Expanded(
+                    flex: 4,
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 16.0),
+                      child: Text(
+                        'Available until',
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.black), // Text color
+                        textAlign: TextAlign.right,
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
                   ),
+                ],
+              ),
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('rooms').limit(7).snapshots(),
+              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+
+                if (snapshot.data!.docs.isEmpty) {
+                  return Center(child: Text('No rooms available.'));
+                }
+
+                final rooms = snapshot.data!.docs
+                    .map((DocumentSnapshot document) {
+                  Map<String, dynamic> roomData = document.data() as Map<String, dynamic>;
+                  return _buildRoomItem(roomData['name'], roomData['building'], roomData['availableUntil']);
+                })
+                    .toList();
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: List.generate(rooms.length, (index) {
+                    final room = rooms[index];
+                    final color = index.isEven ? Color(0xFFD9D9D9) : Color(0xFFEFECEC);
+                    return Container(
+                      color: color,
+                      child: room,
+                    );
+                  }),
+                );
+              },
+            ),
+            SizedBox(height: 8),
+            Spacer(), // Add spacer to move button to the right edge
+            Container(
+              alignment: Alignment.centerRight, // Align the button to the right
+              child: TextButton.icon(
+                onPressed: () {
+                  // Navigate to a page showing all rooms
+                  context.push('/all-rooms');
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color(0xFFBFD5BC)),
+                  foregroundColor: MaterialStateProperty.all(Colors.black),
+                  padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 12, horizontal: 20)), // Adjust button padding
                 ),
-              ],
+                icon: Icon(Icons.arrow_forward),
+                label: Text('See more rooms'),
+              ),
             ),
-          ),
-          StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance.collection('rooms').limit(8).snapshots(),
-            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              }
-
-              if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              }
-
-              if (snapshot.data!.docs.isEmpty) {
-                return Center(child: Text('No rooms available.'));
-              }
-
-              final rooms = snapshot.data!.docs
-                  .map((DocumentSnapshot document) {
-                Map<String, dynamic> roomData = document.data() as Map<String, dynamic>;
-                return _buildRoomItem(roomData['name'], roomData['building'], roomData['availableUntil']);
-              })
-                  .toList();
-
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: List.generate(rooms.length, (index) {
-                  final room = rooms[index];
-                  final color = index.isEven ? Color(0xFFD9D9D9) : Color(0xFFEFECEC);
-                  return Container(
-                    color: color,
-                    child: room,
-                  );
-                }),
-              );
-            },
-          ),
-          SizedBox(height: 8),
-          ElevatedButton(
-            onPressed: () {
-              // Navigate to a page showing all rooms
-              context.push('/all-rooms');
-            },
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Color(0xFFBFD5BC)),
-              foregroundColor: MaterialStateProperty.all(Colors.black),
-              padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 16)),
-            ),
-            child: Text('See more rooms'),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -226,3 +239,5 @@ class TitleSection extends StatelessWidget {
     );
   }
 }
+
+
